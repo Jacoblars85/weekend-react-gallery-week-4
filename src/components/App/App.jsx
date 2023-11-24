0e0ce091f74b180e6f4a3b31aa581b35f28cab73
Jacob Larson <hockeybobber@comcast.net>
2023-11-23 20:35:41 -0600

    added pictures

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -8,7 +8,11 @@ function App() {
         </header>
 
         <p>The gallery goes here!</p>
-        <img src="images/goat_small.jpg"/>
+        <img height={150} width={150} src="images/goat_small.jpg"/>
+        <img height={150} width={150} src="images/EMU1.jpg"/>
+        <img height={150} width={150} src="images/newsdetails.jpeg"/>
+        <img height={150} width={150} src="images/ww-funny-animal-faces-hippopotamus.webp"/>
+        <img height={150} width={150} src="images/50-Funny-Animal-Pictures-That-You-Need-In-Your-Life-2.jpg"/>
       </div>
     );
 }
