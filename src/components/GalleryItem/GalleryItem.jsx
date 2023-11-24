7da0489e05df7aa613bd5c4eea48dc03b66d45e5
Jacob Larson <hockeybobber@comcast.net>
2023-11-23 21:51:52 -0600

    can now toggle the pictures but still not styled correctly

--- src/components/GalleryItem/GalleryItem.jsx
+++ src/components/GalleryItem/GalleryItem.jsx
@@ -1,9 +1,32 @@
+import { useState } from 'react';
+
 function GalleryItem({ pic }) {
+    const [isPicture, setIsPicture] = useState(false)
+
+    const togglePicture = () => {
+      setIsPicture(!isPicture)
+    }
+  
+    const displayText = () => {
+      if (isPicture) {
+        return (
+            <>
+            <p>{pic.description}</p>
+            </>
+        )
+      } else {
+        return (
+            <>
+            <img height={150} width={150} src={pic.url} />
+            </>
+        )
+      }
+    }
     
     return (
-        <>
-        <img height={150} width={150} src={pic.url} />
-        </>
+        <ul onClick={togglePicture}>
+          {displayText()}
+        </ul>
     )
 }
 
