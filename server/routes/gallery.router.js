53227617e2571e48c5346bdc5551026796229a56
Jacob Larson <hockeybobber@comcast.net>
2023-11-23 20:38:38 -0600

    added get route on the server side

--- server/routes/gallery.router.js
+++ server/routes/gallery.router.js
@@ -8,7 +8,17 @@ router.put('/like/:id', (req, res) => {
 
 // GET /gallery
 router.get('/', (req, res) => {
-  // code here
+  const sqlQueryText = `
+  SELECT * FROM "gallery"
+  ORDER BY "id";
+`
+pool.query(sqlQueryText)
+  .then((dbResult) => {
+      console.log('dbResult', dbResult.rows);
+      res.send(dbResult.rows);
+  }).catch((dbError) => {
+      res.sendStatus(500);
+  })
 });
 
 module.exports = router;
